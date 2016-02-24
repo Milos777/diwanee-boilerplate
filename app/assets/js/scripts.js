c0e83d7a745fadacebc9c892b2ed21be90c93659
MilosBjelcevic <milos.bjelcevic@diwanee.com>
2016-02-24 16:39:58 +0100

    first commit

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * diwanee-boilerplate
+ * 
+ * 
+ * @author Diwanee crew
+ * @version 1.0.0
+ * Copyright 2016. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    // FastShell
+  });
+
+})(jQuery, window, document);
